b134f1433cae6bd762a446d4a685191e832cd942
nohpedro <147351155+nohpedro@users.noreply.github.com>
2024-05-23 10:38:54 -0400

    fix

--- FrontendPsicopedagogia/src/components/CentroEstComponente.vue
+++ FrontendPsicopedagogia/src/components/CentroEstComponente.vue
@@ -1,9 +1,9 @@
 <template>
-    <h1>Centro de Estudianteeees</h1>
+  <h1>Centro de Estudianteeees</h1>
 </template>
 
 <script>
 export default {
-    name: "CentroEstComponente",
+  name: "CentroEstComponente",
 };
 </script>
